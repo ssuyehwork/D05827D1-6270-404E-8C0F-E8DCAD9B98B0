dbd87d600faa6eeff612b1455bc3f4478e89184a
ssuyeh <ssuyehwork@gmail.com>
2026-01-13 12:08:49 +0700

    Clean repository

--- core/signals.py
+++
@@ -1,9 +0,0 @@
-# core/signals.py
-from PyQt5.QtCore import QObject, pyqtSignal
-
-class AppSignals(QObject):
-    # 定义一个名为 data_changed 的信号，无参数
-    data_changed = pyqtSignal()
-
-# 创建一个全局单例，方便在应用各处统一调用
-app_signals = AppSignals()
